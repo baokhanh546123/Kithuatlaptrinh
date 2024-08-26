f5e17d5c0aedf6a860187a1a22e0fb8976541375
baokhanh546123 <123302517+baokhanh546123@users.noreply.github.com>
2024-08-26 19:31:33 +0700

    Create oop2_Main.java

---
+++ oop2_Main.java
@@ -0,0 +1,13 @@
+public class Main{
+    public static void main(String[] args) {
+        Phanso A = new Phanso();
+        Phanso B = new Phanso(10);
+        Phanso C = new Phan(7,2);
+        A.show();
+        System.out.print(\n");
+        B.show();
+        System.out.print(\n");
+        C.show();
+        System.out.print(\n");
+    }
+}
