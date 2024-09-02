fd41183ad96d2518a18ac1083e967c6339274ced
baokhanh546123 <123302517+baokhanh546123@users.noreply.github.com>
2024-09-02 20:08:24 +0700

    Update card.java

--- card.java
+++ card.java
@@ -1,24 +1,56 @@
-import java.util.ArrayList;
-import java.util.List;
-
-public class card {
-    String title ; 
-    String author ; 
-    int amount ;
-    List<String[]>store;
-    public card(){
-        store = new ArrayList<>();
-        store.add(new String[]{"Harry Potter","JK Rowling","5"});
-        store.add(new String[]{"Conan","Aoyama Gosho","3"});
-    }
-    public void show(){
-        for (String[]book :store){
-            System.out.print("Title :" + book[0] + " \n Author: " + book[1] + "\n Amout: " + book[2] + "\n ");
-        }
-    }
-    public static void main(String [] args){
-        card cards = new card();
-        cards.show();
-    }
+package Vidu;
+import java.util.Scanner;
+public class Quanli {
+	String title , author;
+	int amount ,size , index , ID; 
+	String [] Q ; 
+	Scanner sc = new Scanner(System.in);
+	
+	public Quanli() {
+		System.out.println("Times: ");
+		this.size = sc.nextInt() ;
+		this.index = 0;
+		this.ID = 1 ;
+		Q = new String [size];
+		sc.nextLine();
+	}
+	
+	public void input() {
+		Scanner sc = new Scanner(System.in);
+		for (int i = 0 ; i < size ; i++) {
+			System.out.print("Enter to title of the book : ");
+			title = sc.nextLine();
+			System.out.print("Enter to author of the book : ");
+			author = sc.nextLine();
+			System.out.print("Enter to amount of the book : ");
+			amount = sc.nextInt();
+			sc.nextLine();
+			store();
+		}
+	}
+	
+	public void store() {
+		if (amount > 0 ) {
+			if (index < Q.length) {
+				Q[index] ="ID:"+ ID + ", Title " + title + ", Author " + author + ", Amount " + amount ;
+				System.out.println("Success");
+				System.out.println(Q[index]);
+				index ++;
+				ID ++;
+			}
+		}else {
+			System.out.println("Failed");
+		}
+		
+	}
+	public void display() {
+		 if (index == 0) {
+	            System.out.println("No books available.");
+	        } else {
+	            System.out.println("List of books:");
+	            for (int i = 0; i < index; i++) {
+	                System.out.println(Q[i]);
+	            }
+	        }
+	}
 }
-
