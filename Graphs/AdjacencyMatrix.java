b9ffe1c93a801e30a2eff48f629b6b72df04c9df
baokhanh546123 <123302517+baokhanh546123@users.noreply.github.com>
2024-11-09 10:49:22 +0700

    Create AdjacencyMatrix.java

---
+++ Graphs/AdjacencyMatrix.java
@@ -0,0 +1,94 @@
+package DSA;
+
+import java.util.*;
+
+public class Graphs {
+	int size;
+	int[][] matrix;
+	String[] vertex_data;
+
+	public Graphs(int size) {
+		this.size = size;
+		this.matrix = new int[size][size];
+		this.vertex_data = new String[size];
+	}
+
+	public void addEgde(int i, int j) {
+		if ((i >= 0 && i < size) && (j >= 0 && j < size)) {
+			matrix[i][j] = 1;
+			matrix[j][i] = 1;
+		} else {
+			System.out.println("Loi");
+			System.exit(0);
+		}
+	}
+
+	public void addVertex(int vertex, String data) {
+		if (vertex >= 0 && vertex < size) {
+			vertex_data[vertex] = data;
+		} else {
+			System.out.println("Loi");
+			System.exit(0);
+		}
+	}
+
+	public void addEgdeWeight(int i, int j) {
+		if ((i >= 0 && i < size) && (j >= 0 && j < size)) {
+			matrix[i][j] = 1;
+		} else {
+			System.out.println("Loi");
+			System.exit(0);
+		}
+	}
+
+	public boolean isConnected(int i, int j) {
+		if (i < 0 || i >= size || j < 0 || j >= size) {
+			return false;
+		}
+		return matrix[i][j] == 1;
+	}
+
+	public void dfs(int v, boolean[] visited) {
+		visited[v] = true;
+		System.out.print(vertex_data[v] + " ");
+		for (int i = 0; i < size; i++) {
+			if (matrix[v][i] == 1 && !visited[i])
+				dfs(i, visited);
+		}
+	}
+
+	public void start_dfs(int startVertex) {
+		boolean[] visited = new boolean[size];
+		dfs(startVertex, visited);
+	}
+
+	public void start_bfs(int startVertex) {
+		boolean[] visited = new boolean[size];
+		Queue<Integer> queue = new LinkedList<>();
+		queue.add(startVertex);
+		visited[startVertex] = true;
+		while (!queue.isEmpty()) {
+			int currentVertex = queue.poll();
+			System.out.print(vertex_data[currentVertex] + " ");
+			for (int i = 0; i < size; i++) {
+				if (matrix[currentVertex][i] != 0 && !visited[i])
+					queue.add(i);
+				visited[i] = true;
+			}
+		}
+	}
+
+	public void display() {
+		System.out.println("Adjacency Matrix :");
+		for (int[] row : matrix) {
+			for (int value : row) {
+				System.out.print(value + " ");
+			}
+			System.out.println();
+		}
+		System.out.println("Vertex Data :");
+		for (int vertex = 0; vertex < size; vertex++) {
+			System.out.printf("Vertex %d: %s%n", vertex, vertex_data[vertex]);
+		}
+	}
+}
