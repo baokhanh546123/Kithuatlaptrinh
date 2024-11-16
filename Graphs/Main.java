b75d0960cd2eb433b4de3350e0006f7a125f4099
baokhanh546123 <123302517+baokhanh546123@users.noreply.github.com>
2024-11-16 15:45:23 +0700

    Update Main.java

--- Graphs/Main.java
+++ Graphs/Main.java
@@ -4,40 +4,46 @@ public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		Graphs graphs = new Graphs(4);
+		Graphs graphs = new Graphs(8);
 		graphs.addVertex(0,"A");
 		graphs.addVertex(1,"B");
 		graphs.addVertex(2,"C");
 		graphs.addVertex(3,"D");
+		graphs.addVertex(4,"E");
+		graphs.addVertex(5,"F");
+		graphs.addVertex(6,"G");
+		graphs.addVertex(7,"K");
 		
-		graphs.addEgde(0,1);
-		graphs.addEgde(1, 3);
-		graphs.addEgde(3, 2);
-		graphs.addEgde(0, 2);
-		graphs.addEgde(2, 1);
-		graphs.addEgde(0,3);
+		graphs.addEdge(0, 1);
+		graphs.addEdge(1, 3);
+		graphs.addEdge(3, 2);
+		graphs.addEdge(0, 2);
+		graphs.addEdge(2, 1);
+		graphs.addEdge(0, 3);
+		graphs.addEdge(4, 5);
+		graphs.addEdge(4, 6);
+		graphs.addEdge(4, 7);
+		graphs.addEdge(5, 6);
+		graphs.addEdge(5, 7);
+		graphs.addEdge(6, 7);
+		graphs.addEdge(0, 4);
+		graphs.addEdge(0, 6);
+		graphs.addEdge(2, 5);
+		graphs.addEdge(2, 7);
+		
+		
 		
 		graphs.display();
 		
 		System.out.println();
-		graphs.start_dfs(3);
+		graphs.dfs(3);
 		System.out.println();
-		graphs.start_bfs(2);
+		graphs.bfs(2);
 		System.out.println();
 		System.out.println(graphs.isConnected(0, 2) ? "Co ket noi " : "Khong ket noi");
 		
-		Graphs_Weight weight = new Graphs_Weight(4);
-		weight.addEgde(0, 1, 5);
-		weight.addEgde(1, 3 ,2);
-		weight.addEgde(3, 2, 3);
-		weight.addEgde(0,2, 4);
-		weight.addEgde(2,1,1);
+		graphs.dijkstra(0);
 		
-		weight.addVertex(0,"A");
-		weight.addVertex(1,"B");
-		weight.addVertex(2,"C");
-		weight.addVertex(3,"D");
-		weight.Print();
 	}
 
 }
