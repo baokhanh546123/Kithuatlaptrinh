122937a121bcd15be3e2c278ab44ce8c534e6f2f
baokhanh546123 <123302517+baokhanh546123@users.noreply.github.com>
2024-11-18 21:05:26 +0700

    Update Main1.java

--- Graphs/Main1.java
+++ Graphs/Main1.java
@@ -1,25 +1,30 @@
-package DSA;
+from AdjacencyMatrixWeight import Graph_Weight
+n = 5 
+graph = Graph_Weight(n)
+graph.addVertex(0, "A")
+graph.addVertex(1, "B")
+graph.addVertex(2, "C")
+graph.addVertex(3, "D")
+graph.addVertex(4, "E")
 
-public class Main1 {
-	public static void main(String[] args) {
-		Graphs_Weight graph = new Graphs_Weight(5);
-		graph.addVertex(0, "A");
-		graph.addVertex(1, "B");
-		graph.addVertex(2, "C");
-		graph.addVertex(3, "D");
-		graph.addVertex(4, "E");
-		graph.addEdge(0, 1, 25);
-		graph.addEdge(0, 2, 22);
-		graph.addEdge(1, 3, 12);
-		graph.addEdge(1, 4, 10);
-		graph.addEdge(2, 4, 6);
-		graph.print();
-		System.out.println("\nDFS bắt đầu từ đỉnh A:");
-		graph.dfs(0);
-		System.out.println("\nBFS bắt đầu từ đỉnh A:");
-		graph.bfs(0);
-		System.out.println("\nChạy thuật toán Dijkstra từ đỉnh A:");
-		graph.dijkstra("B");
+graph.addEdge(0, 1, 10)  
+graph.addEdge(0, 3, 30)  
+graph.addEdge(1, 2, 50)  
+graph.addEdge(2, 3, 20)  
+graph.addEdge(2, 4, 10)  
+graph.addEdge(3, 4, 60)  
 
-	}
-}
+
+graph.printGraph()
+
+print("\nBFS")
+graph.bfs("A")
+
+distances = graph.dijkstra("A")
+print("\nDijkstra:", distances)
+
+start = "B"
+end = "C"
+distance, path = graph.singleDestinationVertex(start, end)
+print(f"\nKhoảng cách ngắn nhất {start} đến {end}: {distance}")
+print(f"Phần từ {n - n } to {n - 1}:", " -> ".join(path))
