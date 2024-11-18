04eaa5c4e3d400db322d42a98238836e2b9d7e30
baokhanh546123 <123302517+baokhanh546123@users.noreply.github.com>
2024-11-18 21:03:35 +0700

    Update Main.py

--- Graphs/Main.py
+++ Graphs/Main.py
@@ -1,8 +1,8 @@
 from AdjacencyMatrix import Graph
-from AdjacencyMatrixWeight import Graph_Weight
+
 if __name__ == '__main__':
     ad =Graph(3)
-    print("Ma tran lien ke khong huong khong trong so ")
+    print("Ma tran liên kề không hướng không trọng số ")
     ad.addEgde(0,1)
     ad.addEgde(0,2)
     ad.addEgde(1,2)
@@ -11,19 +11,7 @@ if __name__ == '__main__':
     ad.addVertex(1,"B")
     ad.addVertex(2,"C")
     ad.display()
-
-
-    weight = Graph_Weight(3)
-    print("Ma tran lien ke co huong co trong so")
-    weight.addEgde(0,1,20)
-    weight.addEgde(0,2,12)
-    weight.addEgde(1,2,39)
-    weight.addEgde(2,1,30)
-    weight.addEgde(2,2,7)
-    
-
-    
-    weight.addVertex(0,"A")
-    weight.addVertex(1,"B")
-    weight.addVertex(2,"C")
-    weight.printGraph()
+    print("Duyệt DFS")
+    ad.dfs("B")
+    print("\nDuyệt BFS")
+    ad.bfs("B")
